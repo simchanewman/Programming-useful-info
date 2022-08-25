76c4dae2389a4ffaddadec308b3611a803b5c710
Simcha_Newman <sss17n@gmail.com>
2022-08-26 00:17:41 +0100

    Added a recursive function for the push

--- git-update.sh
+++ git-update.sh
@@ -6,5 +6,21 @@ git add .
 echo "Add a note for this commit: "
 read commit_note
 git commit -m "$commit_note"
-git push origin -u Main
-echo "Finished"
+git_confirm()
+
+git_confirm()
+{
+    echo "Do you want to proceed to a push to Main? [Y, N]"
+    read commit_confirmation
+    if [[$commit_confirmation -eq "Y"]]
+    then
+        git push origin -u Main
+        echo "Finished with the push"
+    elif [[$commit_confirmation -eq "N"]]
+    then
+        echo "Push cancelled"
+        exit
+    else
+        echo "Only the letter 'Y' or 'N' are accepted as parameters"
+        git_confirm()
+}
